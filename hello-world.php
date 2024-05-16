2b98d162522c500387e1299f41354f9580a53307
Budhi Octaviansyah <budioct@gmail.com>
2024-05-16 16:26:45 +0700

    hello world php web

---
+++ hello-world.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * untuk menjalankan PHP Development Server
+ * ketik:
+ * php -S localhost:8080
+ *
+ * jika berhasil:
+ * [Thu May 16 16:20:52 2024] PHP 8.1.25 Development Server (http://localhost:8080) started
+ *
+ * untuk mengakses file yang kita buat dengan http://localhost:8080/namafile.php
+ * ---> http://localhost:8080/hello-world.php
+ * maka file PHP akan response di halaman web browser
+ *
+ * note:
+ * Best Practice penamaan File PHP.. URL di PHP
+ * ● Gunakan lowercase : misal /product.php, /login.php
+ * ● Jangan gunakan spasi : misal (- strip) /hello-world.php, /forgot-password.php
+ *
+ */
+
+echo "<h1>Hello World!</h1>";
+echo "<h1>Ini adalah Apliasi PHP Web pertama saya!</h1>";
